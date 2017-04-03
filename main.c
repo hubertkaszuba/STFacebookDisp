3ec90f0f1d7ff4bda62a8615e6d2dd0bf8c5fa4d
hubertkaszuba <hubert.kaszuba@gmail.com>
2017-04-03 22:20:40 +0200

    Project

--- main.c
+++ main.c
@@ -16,14 +16,40 @@ int main(void)
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-	unsigned int i;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz ;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP ;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_7 ;
+	GPIO_Init(GPIOA , &GPIO_InitStructure) ;
+
+	// CS
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI1);
+	// SCK
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+	// MISO
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI1, ENABLE);
+
+	SPI_InitTypeDef SPI_InitStructure;
+	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+	SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
+	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256;
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+	SPI_InitStructure.SPI_CRCPolynomial = 7;
+	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+	SPI_Init(SPI1, &SPI_InitStructure);
+
+	SPI_Cmd(SPI1, ENABLE);
+	SPI_SSOutputCmd(SPI1 , ENABLE) ;
+
+	GPIO_WriteBit( SPI_GPIO , SPI1_PIN_NSS , Bit_RESET ) ;
 
 	for(;;)
 	{
-		GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-		for (i=0;i<1000000;i++);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-		for (i=0;i<1000000;i++);
+
 	}
 
 }
